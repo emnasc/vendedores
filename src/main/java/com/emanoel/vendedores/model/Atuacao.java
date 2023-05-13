cc9b899c8f9bada7f6a96ba9ab81619cbde87af8
Emanoel Nascimento <em.nasc@outlook.com>
2023-05-13 19:27:34 -0300

    Initial project structure

---
+++ src/main/java/com/emanoel/vendedores/model/Atuacao.java
@@ -0,0 +1,8 @@
+package com.emanoel.vendedores.model;
+
+import java.util.List;
+
+public class Atuacao {
+    private String regiao;
+    private List<String> estados;
+}
---
+++ src/main/java/com/emanoel/vendedores/model/Vendedor.java
@@ -0,0 +1,17 @@
+package com.emanoel.vendedores.model;
+
+import java.util.Date;
+
+public class Vendedor {
+    private Integer id;
+    private Date dataInclusao;
+
+    private String nome;
+    private String telefone;
+    private Integer idade;
+    private String cidade;
+    private String estado; //TODO: alterar para Enum?
+    private String regiao; //TODO: alterar para Enum?
+
+
+}
